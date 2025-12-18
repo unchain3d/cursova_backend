a063a095d403e903cfe263b613be575803c96dba
unchain3d <ostapmrn@gmail.com>
2025-12-18 22:07:39 +0200

    add files for automated deployment of backend image to gcp

---
+++ install-docker.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -xe
+
+apt update -y
+apt install -y ca-certificates curl gnupg lsb-release
+
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] \
+https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" \
+    | tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt update -y
+apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+systemctl enable docker
+systemctl start docker
+
+usermod -aG docker ubuntu
+
+docker --version || echo "Docker not installed!"
+docker compose version || echo "Compose not installed!"
